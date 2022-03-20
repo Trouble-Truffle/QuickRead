d3d8ec61fa9bfdb3c0e2c12692e27b88c0909119
Trouble-Truffle <perigordtruffle73198@gmail.com>
2022-03-20 20:07:12 +0800

    added speed handling

--- src/QuickRead/Event.hs
+++ src/QuickRead/Event.hs
@@ -3,20 +3,35 @@ module QuickRead.Event where
 
 import           Brick
 import           Control.Lens
+import           Control.Monad
 import           Control.Monad.IO.Class
-import          Control.Monad.Trans.Maybe
+import           Control.Monad.Trans.Maybe
+import           Control.Concurrent.STM
 import qualified Data.Sequence                 as S
 import           Graphics.Vty
 import           QuickRead.Types               as QT
-import Data.Tape as T
+import           Data.Tape                     as T
 
 eventHandler :: Reader -> BrickEvent Name Tick -> EventM Name (Next Reader)
 
-eventHandler game (AppEvent Tick)  =  liftIO (runMaybeT $ step game) >>= 
-  (\case; Nothing -> exit game; Just x -> continue x)
+eventHandler reader (AppEvent Tick)  =  liftIO (runMaybeT $ step reader) >>= 
+  (\case; Nothing -> exit reader; Just x -> continue x)
 
-eventHandler game (VtyEvent (EvKey (KChar 'q') [])) = halt game
-eventHandler game _ = continue game
+eventHandler reader (VtyEvent (EvKey (KChar 'q') [])) = halt reader
+
+eventHandler reader (VtyEvent (EvKey KUp [])) = speedHandler reader (-)
+eventHandler reader (VtyEvent (EvKey KDown [])) = speedHandler reader (+)
+
+eventHandler reader _ = continue reader
+
+
+speedHandler :: Reader -> (Int -> Int -> Int) -> EventM Name (Next Reader)
+speedHandler reader (+/-) = do
+  newSpeed <- liftIO $ (+/-1000) <$> readTVarIO ( reader ^. delay)
+  when (newSpeed > 1000)
+    $ liftIO $ atomically $ writeTVar (reader^.delay) newSpeed
+  continue reader
+
 
 step :: Reader -> MaybeT IO Reader
 step reader = case reader ^. textTape of
